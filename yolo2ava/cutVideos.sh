15c04708925d6787ea32d6a6cc7c8be4917b9e45
Fan Yang <1349896465@qq.com>
2024-02-14 13:25:01 +0800

    Add files via upload

---
+++ yolo2ava/cutVideos.sh
@@ -0,0 +1,15 @@
+IN_DATA_DIR="./videos"
+OUT_DATA_DIR="./ava/trainval_videos_15min"
+
+if [[ ! -d "${OUT_DATA_DIR}" ]]; then
+  echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${OUT_DATA_DIR}
+fi
+
+for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+do
+  out_name="${OUT_DATA_DIR}/${video##*/}"
+  if [ ! -f "${out_name}" ]; then
+    ffmpeg -ss 900 -t 901 -i "${video}" "${out_name}"
+  fi
+done
